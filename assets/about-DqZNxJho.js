8c661fe4f854a678dbdca8616ed07eb578e62a23
buraaks <buraktemur0816@gmail.com>
2026-01-13 20:06:38 +0300

    Updates

---
+++ assets/about-DqZNxJho.js
@@ -0,0 +1,15 @@
+const a=`# Burak Temur\r
+\r
+Benim adım Burak Temur, 17 yaşındayım ve meslek lisesinde 12. sınıf öğrencisiyim. Şuanda yazılım üstüne lise stajımı [TREX Dijital Manufacturing](https://trex.com.tr/tr/)' de yapmaktayım. Amacım yazılımda ve eğitim öğretim hayatımda kendimi geliştirmek.\r
+\r
+## Hobilerim\r
+\r
+- Yazılım Geliştirme\r
+- Sanat\r
+- Yeni Şeyler Öğrenmek\r
+- Vibe coding ile işe yarar açık kaynak proje yapmak. örnek: [OverlayPlayer](https://github.com/buraaks/OverlayPlayer)\r
+\r
+---\r
+\r
+Eğer ortak ilgi alanlarımız varsa, iş görüşmesi yapmak istiyorsanız veya sadece sohbet etmek istiyorsanız benimle iletişime geçebilirsiniz.\r
+`;export{a as default};
